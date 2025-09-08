a910de3cfcc06f0b8bf5750511aabf9da1161e14
Arron John Manlapaz <manlapazarronjohn@gmail.com>
2025-09-08 15:27:02 +0800

    Update projects.blade.php

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -3,11 +3,11 @@
     <div class="mt-12 grid md:grid-cols-2 gap-8 max-w-5xl">
         <a href="https://dreamfactory.ph/" target="_blank" class="p-6 bg-gray-800/70 rounded-xl shadow-lg border border-cyan-400/10 hover:shadow-purple-500/40 hover:-translate-y-2 transition block">
             <h3 class="text-xl font-semibold">PDLC</h3>
-            <p class="mt-3 text-gray-400">A corporate website I developed...</p>
+            <p class="mt-3 text-gray-400">A corporate website I developed for my previous company, focusing on clean design and seamless functionality.</p>
         </a>
         <a href="https://palenke.ph/index.php?page=home&pageID=home" target="_blank" class="p-6 bg-gray-800/70 rounded-xl shadow-lg border border-cyan-400/10 hover:shadow-purple-500/40 hover:-translate-y-2 transition block">
             <h3 class="text-xl font-semibold">PALENKE.PH</h3>
-            <p class="mt-3 text-gray-400">An e-commerce platform I helped develop...</p>
+            <p class="mt-3 text-gray-400"> An e-commerce platform I helped develop for a sister company of my previous employer, where I worked as an outsourced developer. The project aimed to deliver a smooth and efficient online marketplace experience.</p>
         </a>
     </div>
 </section>
